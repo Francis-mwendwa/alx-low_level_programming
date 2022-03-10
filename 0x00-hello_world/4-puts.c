6436de6a7507f7e7d6d0b061ccd535a133a3e738
Francis-mwendwa <fmwendwa536@gmail.com>
2022-03-10 15:14:59 -0800

    hhdhdh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,18 @@
-#!/bin/bash
 #include <stdio.h>
+
+/**
+ *
+ * * main - print the string in the put function
+ *
+ * *
+ *
+ * * Description: using the main function
+ *
+ * * this program prints "Programming is like building a multilingual puzzle
+ *
+ * * Return: 0
+ *
+ * */
 
 int main(void)
 
