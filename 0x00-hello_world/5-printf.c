a411852a81ff7c2f91c7a727bd7ad62f7a2e2ec7
Francis-mwendwa <fmwendwa536@gmail.com>
2022-03-11 07:27:55 -0800

    git again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,10 @@
-#include <stdio.h>
+ #include <stdio.h>
 /**
- *
- *  * main - prints the phrase with proper grammar,
- *
- *   * but the outcome is a piece of art,
- *
- *    * Return: 0 if exited properly, non-zero otherwise
- *
- *     */
-
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
